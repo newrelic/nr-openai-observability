000e0b80c95116fa58fe387b47611dbb73b92be2
nhoffmann-newrelic <123460916+nhoffmann-newrelic@users.noreply.github.com>
2023-10-11 16:18:07 +0200

    Noam/streaming (#26)
    * refactor:extract code to multiple files
    
    * monitor stream for chat completion
    
    * rename file
    
    * async chat completion
    
    * fix stream async + add spans for streams
    
    * remove unused code
    
    * Added stream token calculation
    
    * fix typo + remove prints and comments
    
    * added "stream" attribute to completion events
    
    * handle errors before the stream starts

---
+++ src/nr_openai_observability/consts.py
@@ -0,0 +1,6 @@
+EventName = "LlmCompletion"
+MessageEventName = "LlmChatCompletionMessage"
+SummaryEventName = "LlmChatCompletionSummary"
+EmbeddingEventName = "LlmEmbedding"
+VectorSearchEventName = "LlmVectorSearch"
+VectorSearchResultsEventName = "LlmVectorSearchResult"
