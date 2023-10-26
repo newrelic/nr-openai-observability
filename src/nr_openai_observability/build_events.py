4ea010d063bcc781610f87aa01fd70d804f14293
Ryan Kadri <ryankadri1@gmail.com>
2023-10-26 17:01:37 -0400

    Making SDK attributes match agent (#33)
    * fix: Agent attributes
    
    * feat: Adding compatibility field names
    
    * fix: Bedrock attributes
    
    * fix: PR comments
    
    * fix: Unnecessary import

--- src/nr_openai_observability/build_events.py
+++ src/nr_openai_observability/build_events.py
@@ -18,7 +18,7 @@ def build_messages_events(messages, model, completion_id, tags={}, start_seq_num
             "content": (message.get("content") or "")[:4095],
             "role": message.get("role"),
             "sequence": index + start_seq_num,
-            "model": model,
+            **compat_fields(["model", "response.model"], model),
             "vendor": "openAI",
             "ingest_source": "PythonSDK",
             **get_trace_details(),
@@ -36,13 +36,32 @@ def _get_rate_limit_data(response_headers):
         return int(header) if header and header.isdigit() else None
 
     return {
-        "ratelimit_limit_requests": _get_numeric_header("ratelimit_limit_requests"),
-        "ratelimit_limit_tokens": _get_numeric_header("ratelimit_limit_tokens"),
-        "ratelimit_reset_tokens": response_headers.get("x-ratelimit-reset-tokens"),
-        "ratelimit_reset_requests": response_headers.get("x-ratelimit-reset-requests"),
-        "ratelimit_remaining_tokens": _get_numeric_header("ratelimit_remaining_tokens"),
-        "ratelimit_remaining_requests": _get_numeric_header(
-            "ratelimit_remaining_requests"
+        **compat_fields(
+            ["ratelimit_limit_requests", "response.headers.ratelimitLimitRequests"],
+            _get_numeric_header("ratelimit_limit_requests"),
+        ),
+        **compat_fields(
+            ["ratelimit_limit_tokens", "response.headers.ratelimitLimitTokens"],
+            _get_numeric_header("ratelimit_limit_tokens"),
+        ),
+        **compat_fields(
+            ["ratelimit_reset_tokens", "response.headers.ratelimitResetTokens"],
+            response_headers.get("x-ratelimit-reset-tokens"),
+        ),
+        **compat_fields(
+            ["ratelimit_reset_requests", "response.headers.ratelimitResetRequests"],
+            response_headers.get("x-ratelimit-reset-requests"),
+        ),
+        **compat_fields(
+            ["ratelimit_remaining_tokens", "response.headers.ratelimitRemainingTokens"],
+            _get_numeric_header("ratelimit_remaining_tokens"),
+        ),
+        **compat_fields(
+            [
+                "ratelimit_remaining_requests",
+                "response.headers.ratelimitRemainingRequests",
+            ],
+            _get_numeric_header("ratelimit_remaining_requests"),
         ),
     }
 
@@ -112,18 +131,40 @@ def build_stream_completion_events(
         "response_time": int(response_time * 1000),
         "request.model": request.get("model") or request.get("engine"),
         "response.model": last_chunk.model,
-        "usage.completion_tokens": completion_tokens,
-        "usage.total_tokens": total_tokens,
-        "usage.prompt_tokens": prompt_tokens,
-        "temperature": request.get("temperature"),
-        "max_tokens": request.get("max_tokens"),
-        "finish_reason": last_chunk.choices[0].finish_reason,
-        "api_type": last_chunk.api_type,
+        **compat_fields(
+            ["usage.completion_tokens", "response.usage.completion_tokens"],
+            completion_tokens,
+        ),
+        **compat_fields(
+            ["usage.total_tokens", "response.usage.total_tokens"], total_tokens
+        ),
+        **compat_fields(
+            ["usage.prompt_tokens", "response.usage.prompt_tokens"], prompt_tokens
+        ),
+        **compat_fields(
+            ["temperature", "request.temperature"], request.get("temperature")
+        ),
+        **compat_fields(
+            ["max_tokens", "request.max_tokens"], request.get("max_tokens")
+        ),
+        **compat_fields(
+            ["finish_reason", "response.choices.finish_reason"],
+            last_chunk.choices[0].finish_reason,
+        ),
+        **compat_fields(["api_type", "response.api_type"], last_chunk.api_type),
         "vendor": "openAI",
         "ingest_source": "PythonSDK",
-        "number_of_messages": len(request.get("messages", [])) + 1,
-        "organization": last_chunk.organization,
-        "api_version": response_headers.get("openai-version"),
+        **compat_fields(
+            ["number_of_messages", "response.number_of_messages"],
+            len(request.get("messages", [])) + 1,
+        ),
+        **compat_fields(
+            ["organization", "response.organization"], last_chunk.organization
+        ),
+        **compat_fields(
+            ["api_version", "response.headers.llmVersion"],
+            response_headers.get("openai-version"),
+        ),
         "response": (message.get("content") or "")[:4095],
         "stream": True,
         **get_trace_details(),
@@ -139,22 +180,47 @@ def build_completion_summary(
 ):
     completion = {
         "id": completion_id,
+        "request_id": response_headers.get("x-request-id", ""),
         "api_key_last_four_digits": f"sk-{response.api_key[-4:]}",
         "response_time": int(response_time * 1000),
         "request.model": request.get("model") or request.get("engine"),
         "response.model": response.model,
-        "usage.completion_tokens": response.usage.completion_tokens,
-        "usage.total_tokens": response.usage.total_tokens,
-        "usage.prompt_tokens": response.usage.prompt_tokens,
-        "temperature": request.get("temperature"),
-        "max_tokens": request.get("max_tokens"),
-        "finish_reason": response.choices[0].finish_reason,
-        "api_type": response.api_type,
+        **compat_fields(
+            ["organization", "response.organization"], response.organization
+        ),
+        **compat_fields(
+            ["usage.completion_tokens", "response.usage.completion_tokens"],
+            response.usage.completion_tokens,
+        ),
+        **compat_fields(
+            ["usage.total_tokens", "response.usage.total_tokens"],
+            response.usage.total_tokens,
+        ),
+        **compat_fields(
+            ["usage.prompt_tokens", "response.usage.prompt_tokens"],
+            response.usage.prompt_tokens,
+        ),
+        **compat_fields(
+            ["temperature", "request.temperature"], request.get("temperature")
+        ),
+        **compat_fields(
+            ["max_tokens", "request.max_tokens"], request.get("max_tokens")
+        ),
+        **compat_fields(
+            ["finish_reason", "response.choices.finish_reason"],
+            response.choices[0].finish_reason,
+        ),
+        **compat_fields(["api_type", "response.api_type"], response.api_type),
+        **compat_fields(
+            ["api_version", "response.headers.llmVersion"],
+            response_headers.get("openai-version"),
+        ),
         "vendor": "openAI",
         "ingest_source": "PythonSDK",
-        "number_of_messages": len(request.get("messages", [])) + len(response.choices),
-        "organization": response.organization,
-        "api_version": response_headers.get("openai-version"),
+        **compat_fields(
+            ["number_of_messages", "response.number_of_messages"],
+            len(request.get("messages", [])) + len(response.choices),
+        ),
         "response": (final_message.get("content") or "")[:4095],
         "stream": False,
         **get_trace_details(),
@@ -170,12 +236,19 @@ def build_completion_summary_for_error(request, error, completion_id, isStream=F
         "id": completion_id,
         "api_key_last_four_digits": f"sk-{openai.api_key[-4:]}",
         "request.model": request.get("model") or request.get("engine"),
-        "temperature": request.get("temperature"),
-        "max_tokens": request.get("max_tokens"),
+        **compat_fields(
+            ["temperature", "request.temperature"], request.get("temperature")
+        ),
+        **compat_fields(
+            ["max_tokens", "request.max_tokens"], request.get("max_tokens")
+        ),
         "vendor": "openAI",
         "ingest_source": "PythonSDK",
-        "organization": error.organization,
-        "number_of_messages": len(request.get("messages", [])),
+        **compat_fields(["organization", "response.organization"], error.organization),
+        **compat_fields(
+            ["number_of_messages", "response.number_of_messages"],
+            len(request.get("messages", [])),
+        ),
         "error_status": error.http_status,
         "error_message": error.error.message,
         "error_type": error.error.type,
@@ -193,19 +266,31 @@ def build_embedding_event(response, request, response_headers, response_time):
 
     embedding = {
         "id": embedding_id,
+        "request_id": response_headers.get("x-request-id", ""),
         "input": request.get("input")[:4095],
         "api_key_last_four_digits": f"sk-{response.api_key[-4:]}",
         "timestamp": datetime.now(),
-        "response_time": int(response_time * 1000),
         "request.model": request.get("model") or request.get("engine"),
         "response.model": response.model,
-        "usage.total_tokens": response.usage.total_tokens,
-        "usage.prompt_tokens": response.usage.prompt_tokens,
-        "api_type": response.api_type,
         "vendor": "openAI",
         "ingest_source": "PythonSDK",
-        "organization": response.organization,
-        "api_version": response_headers.get("openai-version"),
+        **compat_fields(["response_time", "duration"], int(response_time * 1000)),
+        **compat_fields(
+            ["usage.total_tokens", "response.usage.total_tokens"],
+            response.usage.total_tokens,
+        ),
+        **compat_fields(
+            ["usage.prompt_tokens", "response.usage.prompt_tokens"],
+            response.usage.prompt_tokens,
+        ),
+        **compat_fields(["api_type", "response.api_type"], response.api_type),
+        **compat_fields(
+            ["api_version", "response.headers.llmVersion"],
+            response_headers.get("openai-version"),
+        ),
+        **compat_fields(
+            ["organization", "response.organization"], response.organization
+        ),
         **get_trace_details(),
     }
 
@@ -223,7 +308,7 @@ def build_embedding_error_event(request, error):
         "request.model": request.get("model") or request.get("engine"),
         "vendor": "openAI",
         "ingest_source": "PythonSDK",
-        "organization": error.organization,
+        **compat_fields(["organization", "response.organization"], error.organization),
         "error_status": error.http_status,
         "error_message": error.error.message,
         "error_type": error.error.type,
@@ -250,3 +335,7 @@ def get_trace_details():
         "transaction_id": transaction_id,
         "transactionId": transaction_id,  # Legacy value from SDK
     }
+
+
+def compat_fields(keys, value):
+    return dict.fromkeys(keys, value)
